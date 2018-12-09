d711bf2b962232102e5ea2959a0b17fa83b144b4
ElenaB <elena.sestrasound@gmail.com>
2018-12-09 20:18:59 +0100

    Translate top level gradle file from Groovy to Kotlin DSL. To avoid errors from Android Studio, keep the Groovy build gradle file renamed.

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+buildscript {
+    repositories {
+        google()
+        jcenter()
+    }
+
+    dependencies {
+        classpath("com.android.tools.build:gradle:3.2.1")
+        classpath( "org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.11")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        jcenter()
+    }
+}
+
+tasks.create("clean", Delete::class.java) { delete(rootProject.buildDir) }
