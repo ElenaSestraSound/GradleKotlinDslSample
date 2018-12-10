26968fcddb2ecdb263baa846ea53ce9ff32d5a9b
ElenaB <elena.sestrasound@gmail.com>
2018-12-10 12:08:12 +0100

    - Generate a new buildSrc module and remove all test files and the buildSrc AndroidManifest.
    - Create the Dependencies.kt file and replace the buildSrc build.gradle by a new build.gradle.kts
    
    - Wrapper gradle update
    
    - Because sync errors, downgrade the gradle version

---
+++ buildSrc/src/main/java/com/harumin/buildsrc/Dependencies.kt
@@ -0,0 +1,9 @@
+object Versions {
+    val kotlin = "1.3.11"
+    val appcompat = "27.1.1"
+}
+
+object Dependencies {
+    val kotlin = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}"
+    val appcompat = "androidx.appcompat:appcompat:${Versions.appcompat}"
+}
